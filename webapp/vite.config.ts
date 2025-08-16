efbe485f6c99d0aae93b524ddf0288a9a1801267
Li Junchen <lijunchen95@outlook.com>
2025-08-16 23:26:16 +0800

    x

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/pyml',
+  base: '/goml',
   plugins: [
     tailwindcss(),
     react(),
