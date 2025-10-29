ce723a1e12b57391826501a35214bf7007646ee8
Li Junchen <lijunchen95@outlook.com>
2025-10-29 18:25:14 +0800

    fix: print ref types using bracket syntax

--- crates/compiler/src/pprint/tast_pprint.rs
+++ crates/compiler/src/pprint/tast_pprint.rs
@@ -200,7 +200,9 @@ impl Ty {
                 .append(RcDoc::text("; "))
                 .append(RcDoc::as_string(len))
                 .append(RcDoc::text("]")),
-            Self::TRef { elem } => RcDoc::text("ref ").append(elem.to_doc(_env)),
+            Self::TRef { elem } => RcDoc::text("Ref[")
+                .append(elem.to_doc(_env))
+                .append(RcDoc::text("]")),
             Self::TFunc { params, ret_ty } => {
                 let mut doc = RcDoc::text("(");
 
