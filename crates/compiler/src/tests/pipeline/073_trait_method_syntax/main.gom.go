0110cfc648857bfe433f2b7550cf990ee54a458e
Li Junchen <lijunchen95@outlook.com>
2026-01-30 08:21:25 +0800

    add and update tests

---
+++ crates/compiler/src/tests/pipeline/073_trait_method_syntax/main.gom.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+    "fmt"
+)
+
+func int32_to_string(x int32) string {
+    return fmt.Sprintf("%d", x)
+}
+
+func string_println(s string) struct{} {
+    fmt.Println(s)
+    return struct{}{}
+}
+
+type S struct {
+    value int32
+}
+
+func _goml_trait_impl_Show_S_show(self__0 S) string {
+    var ret5 string
+    var t3 int32 = self__0.value
+    var t2 string = int32_to_string(t3)
+    var t1 string = "S(" + t2
+    ret5 = t1 + ")"
+    return ret5
+}
+
+func main0() struct{} {
+    var ret6 struct{}
+    var s__1 S = S{
+        value: 7,
+    }
+    var t4 string = _goml_trait_impl_Show_S_show(s__1)
+    string_println(t4)
+    ret6 = struct{}{}
+    return ret6
+}
+
+func main() {
+    main0()
+}
